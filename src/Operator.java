c8c7f73574f7733107f3422dc48963dbf848cecd
박하민 <parkhamint@naver.com>
2023-11-28 05:11:12 +0900

    예제 추가

---
+++ src/Operator.java
@@ -0,0 +1,8 @@
+public class Operator {
+    public static void main(String[] args) {
+        int kor = 77;
+        int eng = 87;
+        System.out.println("총점: "+ (kor+eng));
+        System.out.println("평균: "+ ((kor+eng)/2));
+    }
+}
