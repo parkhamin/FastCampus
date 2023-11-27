86e9309266d6a4dfead326370ed00e768608daa3
박하민 <parkhamint@naver.com>
2023-11-28 05:09:58 +0900

    Initial commit

---
+++ src/Calculator.java
@@ -0,0 +1,16 @@
+// 클래스 단위로 코딩을 한다. -> 객체지향 언어
+// 시작, 메인 클래스
+// JavaSE 프로그램
+public class Calculator {
+    // main(){  } 메서드
+    public static void main(String[] args) {
+        // 두 개의 정수를 더하여 출력하는 자바 프로그램을 만들어 보자
+        int a, b, sum;
+        a = 4;
+        b = 7;
+        sum = a + b;
+        // 출력 : sout
+        System.out.println(sum);
+
+    }
+}
