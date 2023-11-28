cd284693d986266694f881f7687ab8eb2875301f
박하민 <parkhamint@naver.com>
2023-11-29 00:46:38 +0900

    문자자료형

---
+++ src/CharExam.java
@@ -0,0 +1,13 @@
+public class CharExam {
+    public static void main(String[] args) {
+        char a = '0';
+        char b = '1';
+        char c = '2';
+        char d = '3';
+        char e = '4';
+        char f = '5';
+        int sum = (b-a)+(c-a)+(d-a)+(e-a)+(f-a);
+        System.out.println("sum = " + sum);
+
+    }
+}
