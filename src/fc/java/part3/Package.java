62d1ade8d610ac800aa985ceba52d2af500bd455
박하민 <parkhamint@naver.com>
2023-12-04 22:35:28 +0900

    패키지란?

---
+++ src/fc/java/part3/Package.java
@@ -0,0 +1,12 @@
+package fc.java.part3;
+
+/*
+    패키지란? : 서로 기능이 비슷한 클래스들끼리 모아서 관리를 쉽게 하기 위해서 사용 (폴더 개념)
+               패키지 외부에서 클래스의 접근을 할 수 없도록 하기 위해서 사용
+               자바에서 제공해주는 API도 패키지의 형태로 배포됨.
+               ex) java.lang(String, System 등), java.io(입출력 관련), java.net(네트워킹 관련),
+                   java.util(Scanner 등), java.sql(데이터베이스 관련 클래스), java.math(수학 관련 클래스)
+*/
+
+public class Package {
+}
