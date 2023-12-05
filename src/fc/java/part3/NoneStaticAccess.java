06dcf571617ae9efd260f91ac86a21c936d7ac2d
박하민 <parkhamint@naver.com>
2023-12-06 03:12:31 +0900

    static과 none static 멤버들의 접근 방법(추가)

--- src/fc/java/part3/NoneStaticAccess.java
+++ src/fc/java/part3/NoneStaticAccess.java
@@ -6,7 +6,7 @@ public class NoneStaticAccess {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        // MyUtil1
+        // MyUtil1 -> none static이므로 new를 이용해서 객체 생성 해야 함.
         MyUtil1 mu = new MyUtil1();
         int sum = mu.hap(a, b);
         System.out.println("sum = " + sum);
--- src/fc/java/part3/StaticAccess.java
+++ src/fc/java/part3/StaticAccess.java
@@ -6,7 +6,8 @@ public class StaticAccess {
     public static void main(String[] args) {
         int a = 10;
         int b = 20;
-        // MyUtil
+        // MyUtil -> static 멤버는 클래스를 사용하는 시점에서 자동으로 static-zone에 로딩된다.
+        // 따라서 new를 이용해서 객체를 생성할 필요가 없다.
         int sum = MyUtil.hap(a,b); // 클래스이름.호출메서드
         System.out.println("sum = " + sum);
     }
